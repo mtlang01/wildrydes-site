7628afecea35d5fe84aab4498e5bee3d62de0217
Matthew <matthew.lang.t@gmail.com>
2025-06-10 12:02:39 -0400

    added pool and client ID

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_saI6I7hQ7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'g68aa7nb1vto5mv73bgetlotl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
